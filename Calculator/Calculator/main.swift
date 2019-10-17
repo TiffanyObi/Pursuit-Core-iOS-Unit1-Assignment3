207a50ed4a6713defb6b3ebbfa12674896155dfc
Tiffany Obi <tiffanyobi-pursuit@Tiffanys-MacBook-Pro.local>
2019-10-17 17:05:28 -0400

    got the operaters working as well as the random function

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -9,26 +9,89 @@
 import Foundation
 
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
-  switch opString {
+    var opString = opString
+    if opString == "?" {
+        opString = ["/", "+", "*", "-"].randomElement() ?? "*"
+    }
     
-  case "+":
-    return {x, y in x + y}
-  case "-":
-    
-    return {x, y in x - y}
-  case "*":
-    
-    return {x, y in x * y}
-  case "/":
-    
-    return {x, y in x / y}
-  default:
-    
-    return {x, y in x + y}
-  }
+    switch opString {
+        
+    case "+":
+        return {x, y in x + y}
+    case "-":
+        
+        return {x, y in x - y}
+    case "*":
+        
+        return {x, y in x * y}
+    case "/":
+        
+        return {x, y in x / y}
+    default:
+        
+        return {x, y in x + y}
+    }
 }
 
-let closureOperation = mathStuffFactory(opString: "+")
+func arithmeticOperation (str: String) -> Double {
+    
+    let userInputArray = str.components(separatedBy: " " )
+    
+    if userInputArray.count != 3 {
+        
+        print(userInputArray)
+        
+        return 0.0
+    }
+    let op1 = Double(userInputArray[0]) ?? 0.0
+    let op2 = Double(userInputArray[2]) ?? 0.0
+    let operater = userInputArray[1]
+    
+    
+    let closureOp = mathStuffFactory(opString: operater)
+    let result = closureOp(op1,op2)
 
-let result = closureOperation(45,5)
-print("result of operation \(result)")
+    return result
+}
+
+var userIsUsing = true
+
+//var userInput = readLine() ?? ""
+
+
+//let finalResult = arithmeticOperation(str: userInput)
+//print(finalResult)
+
+
+repeat {
+    
+    
+    print("Please type in your calculation with a space in between. for example: \"5 + 5\". ")
+    
+    var userInput = readLine() ?? ""
+    
+    var finalResult = arithmeticOperation(str:userInput)
+    print(finalResult)
+
+    print("Would you like to do another calculation? Type yes or no")
+    
+    userInput = readLine() ?? ""
+    
+    if userInput.lowercased() == "yes" {
+        
+        print("Lets have some fun!If you would like to use our \"Random Operater\" gadget ,write a calculation usuing \"?\" as the operand. ex: \" 5 ? 5 \"  ")
+        
+        userInput = readLine() ?? ""
+        
+        finalResult = arithmeticOperation(str:userInput)
+           print(finalResult)
+        
+    } else if userInput.lowercased() == "no"{
+        print(" You're Calculater is shutting off")
+        
+        userIsUsing = false
+    }
+    
+} while userIsUsing
+
+
